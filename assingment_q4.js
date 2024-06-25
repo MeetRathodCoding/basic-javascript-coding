dd2d7abb2ceb7d08e00e89385b05807c210a8452
MeetRathodCoding <meethrathod85@gmail.com>
2024-06-25 23:46:28 +0530

    Add files via upload

---
+++ assingment_q4.js
@@ -0,0 +1,7 @@
+for(i=1;i<=20;i++){//for loop passed initilizer,condition,increment by 1
+    if(i%2!=0)//condintion in if statement i modulous 2 is not equal to 0 
+        {
+            continue;//continue statement to skip iteration
+        }
+    console.log(i);//log i
+}
